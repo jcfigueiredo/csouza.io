5731ecd2bac35dc67db186abe0e5dfa188e4c9c5
Claudio Souza <claudio@codecademy.com>
2020-09-17 23:01:33 -0400

    adds google tag manager

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -14,6 +14,12 @@ const TemplateWrapper = ({ children }) => {
         <html lang="en" />
         <title>{title}</title>
         <meta name="description" content={description} />
+        
+        <script>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-MG5M3ZZ');`}</script>
 
         <link
           rel="apple-touch-icon"
